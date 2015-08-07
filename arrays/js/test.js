e985dc4b1278627d4c28777cf617c2e83774ae80
Lakshman Parameswaran <lparameswaran@g.harvard.edu>
2015-08-07 18:05:31 -0400

    with pop and length

--- arrays/js/test.js
+++ arrays/js/test.js
@@ -1,11 +1,18 @@
 function initialize() {
   var a = [5, 500, 20, 21, 22];
   console.log("Testing", a);
+  console.log("Array Length", a.length);
 
   a.push(6);
   console.log("Testing New array", a);
+  console.log("Array Length", a.length);
 
   var t1 = a.pop();
+  console.log("Array Length", a.length);
   var t2 = a.pop();
+  console.log("Array Length", a.length);
   console.log("Popped", t1, t2, a);
+
+  // Check the length of the array
+  console.log("Array Length", a.length);
 }
